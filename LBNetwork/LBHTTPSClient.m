c7928ecfd5c156719f75ff41dfe8b6fde010f182
Lena Brusilovski <lenabru@gmail.com>
2015-09-30 13:26:57 +0300

    update content type

--- LBNetwork/LBHTTPSClient.m
+++ LBNetwork/LBHTTPSClient.m
@@ -30,7 +30,7 @@ NSString* const kMethodPUT = @"PUT";
 NSString* const kMethodDELETE = @"DELETE";
 
 NSString* const ContentTypeAutomatic = @"jsonmodel/automatic";
-NSString* const ContentTypeJSON = @"application/json;charset=UTF-8";
+NSString* const ContentTypeJSON = @"application/json";
 NSString* const ContentTypeWWWEncoded = @"application/x-www-form-urlencoded";
 
 NSString* const DataContentTypeImage = @"image/jpeg";
